74817622123c8272715e1a6e1cd182593c60a4a7
KP Dorsey <dorsey_k17@hotmail.com>
2026-03-25 13:37:08 -0500

    Adding CIPCodes lookup table

--- higher-ed_schemas.sql
+++ higher-ed_schemas.sql
@@ -1,42 +1,42 @@
 CREATE TABLE IF NOT EXISTS edu_c20yy_a (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  UNITID varchar(6),
-  CIPCODE numeric(12,6),
-  MAJORNUM smallint,
-  AWLEVEL smallint,
-  CTOTALT smallint,
-  YEAR varchar(4)
+  id bigint generated always as identity primary key,
+  unitid varchar(6),
+  cipcode varchar(7),
+  majornum smallint,
+  awlevel smallint,
+  ctotalt smallint,
+  year varchar(4)
 );
 
 
 CREATE TABLE IF NOT EXISTS edu_hd20yy (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  UNITID varchar(6),
-  INSTNM varchar(120),
-  IALIAS varchar(2000),
-  ADDR varchar(100),
-  CITY varchar(30),
-  STABBR varchar(2),
-  ZIP varchar(10),
-  FIPS varchar(2),
-  SECTOR varchar(2),
-  ICLEVEL varchar(2),
-  CONTROL varchar(2),
-  HLOFFER varchar(2),
-  UGOFFER varchar(2),
-  GROFFER varchar(2),
-  HDEGOFR1 varchar(2),
-  DEGGRANT varchar(2),
-  COUNTYCD varchar(5),
-  COUNTYNM varchar(60),
-  CNGDSTCD varchar(4),
-  LONGITUD numeric(12,6),
-  LATITUDE numeric(12,6)
+  id bigint generated always as identity primary key,
+  unitid varchar(6),
+  instnm varchar(120),
+  ialias varchar(2000),
+  addr varchar(100),
+  city varchar(30),
+  stabbr varchar(2),
+  zip varchar(10),
+  fips varchar(2),
+  sector varchar(2),
+  iclevel varchar(2),
+  control varchar(2),
+  hloffer varchar(2),
+  ugoffer varchar(2),
+  groffer varchar(2),
+  hdegofr1 varchar(2),
+  deggrant varchar(2),
+  countycd varchar(5),
+  countynm varchar(60),
+  cngdstcd varchar(4),
+  longitud numeric(12,6),
+  latitude numeric(12,6)
 );
 
 
 CREATE TABLE IF NOT EXISTS empl_singlefile_qtrly(
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  id bigint generated always as identity primary key,
   area_fips varchar(5),
   own_code varchar(1),
   industry_code varchar(6),
@@ -45,20 +45,38 @@ CREATE TABLE IF NOT EXISTS empl_singlefile_qtrly(
   year varchar(4),
   qtr varchar(1),
   disclosure_code varchar(1),
-  qtrly_estabs Numeric(9),
-  month3_emplvl Numeric(9)
+  qtrly_estabs numeric(9),
+  month3_emplvl numeric(9)
 );
 
 
 CREATE TABLE IF NOT EXISTS empl_industry_titles(
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  id bigint generated always as identity primary key,
   industry_code varchar(6),
   industry_title varchar(150)
 );
 
 
 CREATE TABLE IF NOT EXISTS empl_ownership_titles(
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  id bigint generated always as identity primary key,
   own_code varchar(1),
   own_title varchar(150)
 );
+
+
+CREATE TABLE IF NOT EXISTS edu_cipcodes_2020(
+  id bigint generated always as identity primary key,
+  cipfamily varchar(2),
+  cipcode varchar(7),
+  action varchar(50),
+  textchange boolean,
+  ciptitle varchar(150),
+  cipdefinition text,
+  crossreferences text,
+  examples text
+);
+
+
+
+
+
